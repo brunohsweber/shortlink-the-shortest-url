25dd34f47c05c52670480338460bb98dc757ca15
Bruno Weber <contato@brunoweber.com.br>
2022-03-17 16:29:31 -0300

    chore: add algorithm to generate the short url

--- src/modules/shortlink/utils/GenerateCode.ts
+++ src/modules/shortlink/utils/GenerateCode.ts
@@ -1,5 +1,21 @@
 class GenerateCode {
 
+  private code: string;
+
+  constructor() {
+    this.code = "";
+  }
+
+  public async execute(): Promise<string> {
+
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    for (let i = 0; i < 5; i++) {
+      this.code += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+
+    return this.code;
+  }
 }
 
 export { GenerateCode };
