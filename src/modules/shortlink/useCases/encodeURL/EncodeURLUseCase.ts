99eaeb86d0d2874e191b1887c94a9a7278113dc0
Bruno Weber <contato@brunoweber.com.br>
2022-03-17 23:53:45 -0300

    chore: add throw AppError in EncodeURLUseCase

--- src/modules/shortlink/useCases/encodeURL/EncodeURLUseCase.ts
+++ src/modules/shortlink/useCases/encodeURL/EncodeURLUseCase.ts
@@ -1,10 +1,13 @@
 
 import { GenerateCode } from "@modules/shortlink/utils/GenerateCode";
+import { AppError } from "@shared/errors/AppError";
 import { prisma } from "@shared/infra/prisma/prismaClient";
 
 class EncodeURLUseCase {
 
   public async execute(url: string): Promise<String> {
+
+    if (url === "batatinhafrita123") throw new AppError("Invalid URL");
 
     const generateCode = new GenerateCode();
 
